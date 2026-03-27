b075b7e9ea8d12328283e55cfaad0a38bd172bf0
vp13t <vpietropaolo1998@gmail.com>
2026-03-27 12:04:44 -0600

    Disable camera display

--- src/camera_test_pkg/camera_test_pkg/my_color_sub.py
+++ src/camera_test_pkg/camera_test_pkg/my_color_sub.py
@@ -125,9 +125,9 @@ class ImageListener(Node):
 
             
 #--------------------testing display------------------------
-        cv2.imshow("Intel RealSense Camera", frame) 
-        cv2.imshow("mask", mask)
-        cv2.waitKey(1) 
+        # cv2.imshow("Intel RealSense Camera", frame) 
+        # cv2.imshow("mask", mask)
+        # cv2.waitKey(1) 
         
     def update_params(self):
 
