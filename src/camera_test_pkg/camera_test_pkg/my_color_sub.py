34db40f83e2f03f74ae3f496c1cb8ff26231b80e
kranzowe <kranzowe@gmail.com>
2026-03-27 12:05:01 -0600

    no show

--- src/camera_test_pkg/camera_test_pkg/my_color_sub.py
+++ src/camera_test_pkg/camera_test_pkg/my_color_sub.py
@@ -119,9 +119,9 @@ class ImageListener(Node):
 
             
 #--------------------testing display------------------------
-        cv2.imshow("Intel RealSense Camera", frame) 
-        cv2.imshow("mask", mask)
-        cv2.waitKey(1) 
+        # cv2.imshow("Intel RealSense Camera", frame) 
+        # cv2.imshow("mask", mask)
+        # cv2.waitKey(1) 
         
     def update_params(self):
 
