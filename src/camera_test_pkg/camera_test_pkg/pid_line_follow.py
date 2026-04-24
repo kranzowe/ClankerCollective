4ac4ea57881356d3db066bcbb1bf4902037e2dbb
Sam Zanotti <saza2945@colorado.edu>
2026-04-24 15:00:54 -0600

    added delay to fresh_path_flag

--- src/camera_test_pkg/camera_test_pkg/pid_line_follow.py
+++ src/camera_test_pkg/camera_test_pkg/pid_line_follow.py
@@ -120,9 +120,9 @@ class LineFollower(Node):
             if self.steps_right_turn < 25 and not self.fresh_path_flag:
                 self.steps_right_turn += 1
                 if self.steps_right_turn >15 and self.has_fresh_path:  #want to get into the turn before looking for fresh path
+                    for i in range(3):
+                        None
                     self.fresh_path_flag = True
-                self.get_logger().info(self.has_fresh_path)
-                self.get_logger().info(self.steps_right_turn)
             else:
                 self.right_turn_detected = False
                 self.steps_right_turn = 0
