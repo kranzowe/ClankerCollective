38e4c4cbdaf4bedf606aa5b096d4aaa9deedaf16
kranzowe <kranzowe@gmail.com>
2026-04-28 13:48:44 -0600

    uncommented right turn alg

--- src/camera_test_pkg/camera_test_pkg/pid_line_follow.py
+++ src/camera_test_pkg/camera_test_pkg/pid_line_follow.py
@@ -57,21 +57,21 @@ class LineFollower(Node):
             if np.isfinite(pose.theta):
                 thetas.append(pose.theta)
         
-        # if all(np.isfinite([pose.theta for pose in msg.poses[-4:]])):
-        #     # Check if all final poses are horizontally aligned (within tolerance)
-        #     final_poses = msg.poses[-3:]
-        #     y_tolerance = 0.0  # adjust based on your image scale
-        #     first_y = final_poses[0].y
-        #     y_aligned = all(abs(pose.y - first_y) < y_tolerance for pose in final_poses)
+        if all(np.isfinite([pose.theta for pose in msg.poses[-4:]])):
+            # Check if all final poses are horizontally aligned (within tolerance)
+            final_poses = msg.poses[-3:]
+            y_tolerance = 0.0  # adjust based on your image scale
+            first_y = final_poses[0].y
+            y_aligned = all(abs(pose.y - first_y) < y_tolerance for pose in final_poses)
             
-        #     if y_aligned:
-        #         if not self.right_turn_detected:
-        #             self.get_logger().info('RIGHT TURN RIGHT TURN RIGHT TURN')
-        #             self.right_turn_detected = True
-        #             self.steps_right_turn = 0
-        #         thetas.append(np.pi / 2)  # bias it rightwards
-        #     else:
-        #         self.get_logger().info(f'Not horizontal: y spread = {max(p.y for p in final_poses) - min(p.y for p in final_poses):.3f}')
+            if y_aligned:
+                if not self.right_turn_detected:
+                    self.get_logger().info('RIGHT TURN RIGHT TURN RIGHT TURN')
+                    self.right_turn_detected = True
+                    self.steps_right_turn = 0
+                thetas.append(np.pi / 2)  # bias it rightwards
+            else:
+                self.get_logger().info(f'Not horizontal: y spread = {max(p.y for p in final_poses) - min(p.y for p in final_poses):.3f}')
 
         if not thetas:
             now = time.monotonic()
