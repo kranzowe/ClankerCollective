aed5514100af3684b951899b8a0f400fd5960aca
Sam Zanotti <saza2945@colorado.edu>
2026-04-30 17:41:10 -0600

    million

--- src/camera_test_pkg/camera_test_pkg/pid_line_follow_stopsign.py
+++ src/camera_test_pkg/camera_test_pkg/pid_line_follow_stopsign.py
@@ -80,7 +80,7 @@ class LineFollower(Node):
 
         if all(np.isfinite([pose.theta for pose in msg.poses[-4:]])):
             final_poses = msg.poses[-3:]
-            y_tolerance = 10
+            y_tolerance = 100
             first_y = final_poses[0].y
             y_aligned = all(abs(pose.y - first_y) < y_tolerance for pose in final_poses)
 
