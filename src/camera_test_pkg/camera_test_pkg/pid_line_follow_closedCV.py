e0543f0c0132c7e31e3d12b8a846cdccd9b808ec
Bill Hsu <bill10361239@gmail.com>
2026-05-01 08:41:04 -0600

    try

--- src/camera_test_pkg/camera_test_pkg/pid_line_follow_closedCV.py
+++ src/camera_test_pkg/camera_test_pkg/pid_line_follow_closedCV.py
@@ -26,8 +26,8 @@ TURN_BIAS = 1480.0
 # steer: steering offset added to TURN_BIAS (negative = right, positive = left)
 # speed: linear.x PWM during the turn
 TURN_CONFIGS = [
-    {"steps": 30, "steer": -300.0, "speed": DEFAULT_SPEED},  # turn 1
-    {"steps": 30, "steer": -300.0, "speed": DEFAULT_SPEED},  # turn 2
+    {"steps": 50, "steer": -300.0, "speed": DEFAULT_SPEED},  # turn 1
+    {"steps": 50, "steer": -300.0, "speed": DEFAULT_SPEED},  # turn 2
     {"steps": 30, "steer": -300.0, "speed": DEFAULT_SPEED},  # turn 3
 ]
 
